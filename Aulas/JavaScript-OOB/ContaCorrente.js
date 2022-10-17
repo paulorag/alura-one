e5d3076ec4ecfea62533f160f8efd2638cfcad46
Paulo Roberto <89588460+paulorag@users.noreply.github.com>
2022-10-17 09:14:25 -0300

    att

--- Aulas/JavaScript-OOB/ContaCorrente.js
+++ Aulas/JavaScript-OOB/ContaCorrente.js
@@ -1,9 +1,25 @@
+import { Cliente } from "./Cliente.js";
+
 export class ContaCorrente {
     agencia;
-    cliente;
+    _cliente;
 
     // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
     _saldo = 0;
+
+    set cliente(novoValor) {
+        if (novoValor instanceof Cliente) {
+            this._cliente = novoValor;
+        }
+    }
+
+    get cliente() {
+        return this._cliente;
+    }
+
+    get saldo() {
+        return this._saldo;
+    }
 
     sacar(valor) {
         if (this._saldo >= valor) {
--- Aulas/JavaScript-OOB/index.js
+++ Aulas/JavaScript-OOB/index.js
@@ -1,13 +1,13 @@
-import { Cliente } from "./Cliente.js.js";
+import { Cliente } from "./Cliente.js";
 import { ContaCorrente } from "./ContaCorrente.js";
 
 const cliente1 = new Cliente();
 cliente1.nome = "Paulo";
 cliente1.cpf = 11122233309;
 
-const cliente2 = new Cliente();
-cliente2.nome = "Byanca";
-cliente2.cpf = 88822233309;
+// const cliente2 = new Cliente();
+// cliente2.nome = "Byanca";
+// cliente2.cpf = 88822233309;
 
 const contaCorrentePaulo = new ContaCorrente();
 contaCorrentePaulo.agencia = 1001;
@@ -15,11 +15,9 @@ contaCorrentePaulo.cliente = cliente1;
 contaCorrentePaulo.depositar(500);
 
 const conta2 = new ContaCorrente();
-conta2.cliente = cliente2;
+conta2.cliente = cliente1;
 conta2.agencia = 102;
 
 let valor = 200;
 contaCorrentePaulo.tranferir(valor, conta2);
-
-console.log("valor: ", valor);
-console.log(conta2);
+console.log(conta2.saldo);
