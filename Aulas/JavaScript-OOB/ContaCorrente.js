9d042b6c8605a2741147acca21052d887c25e9c8
Paulo Roberto <89588460+paulorag@users.noreply.github.com>
2022-10-17 09:43:11 -0300

    att

--- Aulas/JavaScript-OOB/ContaCorrente.js
+++ Aulas/JavaScript-OOB/ContaCorrente.js
@@ -1,6 +1,7 @@
 import { Cliente } from "./Cliente.js";
 
 export class ContaCorrente {
+    static numeroDeContas = 0;
     agencia;
     _cliente;
 
@@ -19,6 +20,12 @@ export class ContaCorrente {
 
     get saldo() {
         return this._saldo;
+    }
+
+    constructor(cliente, agencia) {
+        this.agencia = agencia;
+        this.cliente = cliente;
+        ContaCorrente.numeroDeContas += 1;
     }
 
     sacar(valor) {
--- Aulas/JavaScript-OOB/index.js
+++ Aulas/JavaScript-OOB/index.js
@@ -1,23 +1,14 @@
 import { Cliente } from "./Cliente.js";
 import { ContaCorrente } from "./ContaCorrente.js";
 
-const cliente1 = new Cliente();
-cliente1.nome = "Paulo";
-cliente1.cpf = 11122233309;
+const cliente1 = new Cliente('Paulo', 11122233309);
+const cliente2 = new Cliente("Byanca", 88822233309);
 
-// const cliente2 = new Cliente();
-// cliente2.nome = "Byanca";
-// cliente2.cpf = 88822233309;
-
-const contaCorrentePaulo = new ContaCorrente();
-contaCorrentePaulo.agencia = 1001;
-contaCorrentePaulo.cliente = cliente1;
+const contaCorrentePaulo = new ContaCorrente(cliente1, 1001);
 contaCorrentePaulo.depositar(500);
-
-const conta2 = new ContaCorrente();
-conta2.cliente = cliente1;
-conta2.agencia = 102;
+const conta2 = new ContaCorrente(cliente2, 102);
 
 let valor = 200;
 contaCorrentePaulo.tranferir(valor, conta2);
-console.log(conta2.saldo);
+
+console.log(ContaCorrente.numeroDeContas);
