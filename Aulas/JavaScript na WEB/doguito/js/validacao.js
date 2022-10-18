f08dcabdfd8ef7ad54283d493636c563b55198d1
Paulo Roberto <89588460+paulorag@users.noreply.github.com>
2022-10-18 09:07:20 -0300

    att

---
+++ Aulas/JavaScript na WEB/doguito/js/validacao.js
@@ -0,0 +1,68 @@
+export function valida(input) {
+    const tipoDeInput = input.dataset.tipo;
+
+    if (validadores[tipoDeInput]) {
+        validadores[tipoDeInput](input);
+    }
+
+    if (input.validity.valid) {
+        input.parentElement.classList.remove('input-container--invalido');
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = '';
+    } else {
+        input.parentElement.classList.add('input-container--invalido');
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = mostraMensagemDeErro(tipoDeInput, input);
+    }
+}
+
+const tiposDeErro = ['valueMissing', 'typeMismatch', 'patternMismatch', 'customError'];
+
+const mensagensDeErro = {
+    nome: {
+        validMissing: 'O campo nome não pode estar vazio!'
+    },
+    email: {
+        validMissing: 'O campo de e-mail não pode estar vazio!',
+        typeMismatch: 'O e-mail digitado não é válido!'
+    },
+    senha: {
+        validMissing: 'O campo de senha não pode estar vazio!',
+        patternMismatch: 'A senha deve conter entre 6 a 12 caracteres, deve conter pelo menos uma letra maiuscula, um número e não deve conter simbolos.'
+    },
+    dataNascimento: {
+        validMissing: 'O campo de data de nascimento não pode estar vazio!',
+        customError: 'Você deve ter mais de 18 anos para se cadastrar!'
+    }
+}
+
+const validadores = {
+    dataNascimento: input => validaDataNascimento(input)
+}
+
+function mostraMensagemDeErro(tipoDeInput, input) {
+    let mensagem = '';
+    tiposDeErro.forEach(erro => {
+        if (input.validity[erro]) {
+            mensagem = mensagensDeErro[tipoDeInput][erro];
+        }
+    });
+
+    return mensagem;
+}
+
+function validaDataNascimento(input) {
+    const dataRecebida = new Date(input.value);
+    let mensagem = '';
+
+    if (!maiorQue18(dataRecebida)) {
+        mensagem = 'Você deve ter mais de 18 anos para se cadastrar!';
+    }
+
+    input.setCustomValidity(mensagem);
+}
+
+function maiorQue18(data) {
+    const dataAtual = new Date();
+    const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate());
+
+    return dataMais18 <= dataAtual;
+}
