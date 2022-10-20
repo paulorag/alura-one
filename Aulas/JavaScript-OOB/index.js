7a29e4edfd7a7cfaf710d4d1188ff919f0e03bcc
Paulo Roberto <89588460+paulorag@users.noreply.github.com>
2022-10-20 09:41:44 -0300

    att

--- Aulas/JavaScript-OOB/index.js
+++ Aulas/JavaScript-OOB/index.js
@@ -1,26 +1,16 @@
 import { Cliente } from "./Cliente.js";
-import { ContaCorrente } from "./Conta/ContaCorrente.js";
-import { ContaPoupanca } from "./Conta/ContaPoupanca.js";
-import { ContaSalario } from "./Conta/ContaSalario.js";
+import { Gerente } from "./Funcionario/Gerente.js";
+import { Diretor } from "./Funcionario/Diretor.js";
+import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 
-const cliente1 = new Cliente('Paulo', 11122233309);
-const cliente2 = new Cliente('Byanca', 88822233309);
+const diretor = new Diretor('Paulo', 10000, 12345678900);
+diretor.cadastrarSenha('123456');
+const gerente = new Gerente('Byanca', 5000, 12345674500);
+gerente.cadastrarSenha('123');
+const cliente = new Cliente('Aa', 123456789, '456');
 
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, '123456');
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente, '123');
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, '456');
 
-const contaCorrentePaulo = new ContaCorrente(cliente1, 1001);
-contaCorrentePaulo.depositar(500);
-contaCorrentePaulo.sacar(100);
-
-console.log(contaCorrentePaulo);
-
-const contaPoupancaByanca = new ContaPoupanca(50, cliente2, 1001);
-contaPoupancaByanca.depositar(100);
-contaPoupancaByanca.sacar(10);
-
-console.log(contaPoupancaByanca);
-
-const contaSalario = new ContaSalario(cliente1);
-contaSalario.depositar(100);
-contaSalario.sacar(10);
-
-console.log(contaSalario);
+console.log(gerenteEstaLogado, diretorEstaLogado, clienteEstaLogado);
