9f78cad50663bc88a61ed467732bb4a6c53d82aa
Paulo Roberto <89588460+paulorag@users.noreply.github.com>
2022-10-20 09:47:58 -0300

    att

--- Aulas/JavaScript DOM/main.js
+++ Aulas/JavaScript DOM/main.js
@@ -1,26 +1,27 @@
-import BotaoConclui from "./componentes/concluiTarefa.js";
-import BotaoDeleta from "./componentes/deletaTarefa.js";
+import BotaoConclui from './componentes/concluiTarefa.js'
+import BotaoDeleta from './componentes/deletaTarefa.js'
 
 const criarTarefa = (evento) => {
-    evento.preventDefault();
 
-    const lista = document.querySelector('[data-list]');
-    const input = document.querySelector('[data-form-input]');
-    const valor = input.value;
+    evento.preventDefault()
 
-    const tarefa = document.createElement('li');
-    tarefa.classList.add('task');
+    const lista = document.querySelector('[data-list]')
+    const input = document.querySelector('[data-form-input]')
+    const valor = input.value
 
-    const conteudo = `<p class="content">${valor}</p>`;
+    const tarefa = document.createElement('li')
+    tarefa.classList.add('task')
+    const conteudo = `<p class="content">${valor}</p>`
 
-    tarefa.innerHTML = conteudo;
+    tarefa.innerHTML = conteudo
 
-    tarefa.appendChild(BotaoConclui());
-    tarefa.appendChild(BotaoDeleta());
-    lista.appendChild(tarefa);
-    input.value = '';
+    tarefa.appendChild(BotaoConclui())
+    tarefa.appendChild(BotaoDeleta())
+    lista.appendChild(tarefa)
+    input.value = " "
+
 }
 
-const novaTarefa = document.querySelector('[data-form-button]');
+const novaTarefa = document.querySelector('[data-form-button]')
 
-novaTarefa.addEventListener('click', criarTarefa);
+novaTarefa.addEventListener('click', criarTarefa)
