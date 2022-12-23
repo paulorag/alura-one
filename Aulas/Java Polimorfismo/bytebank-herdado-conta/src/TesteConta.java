138939ef59e242ad1d8b063bf59364033d4fde66
Paulo Roberto <89588460+paulorag@users.noreply.github.com>
2022-12-23 18:41:16 -0300

    att

---
+++ Aulas/Java Polimorfismo/bytebank-herdado-conta/src/TesteConta.java
@@ -0,0 +1,15 @@
+
+public class TesteConta {
+	public static void main(String[] args) {
+		ContaCorrente cc = new ContaCorrente(111, 111);
+		cc.deposita(100);
+
+		ContaPoupanca cp = new ContaPoupanca(222, 222);
+		cp.deposita(200);
+
+		cc.transfere(10, cp);
+
+		System.out.println("CC R$" + cc.getSaldo());
+		System.out.println("CP R$" + cp.getSaldo());
+	}
+}
